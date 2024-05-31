b1124663715163f585f04c338ead8673840049a7
cjporter25 <chris.j.porter25@gmail.com>
2024-05-31 01:00:01 -0500

    Intro

---
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,15 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  // Declare a value with the keyword: "val" then [nameOfVar]:
+  //  Following that, fill in the correct file type
+  val meaningOfLife: Int = 42
+
+
+  //Any object with a "main" function can be compiled
+  //  and ran on its own
+  def main(args: Array[String]): Unit = {
+
+  }
+}
