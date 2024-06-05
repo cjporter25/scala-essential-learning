b2e715524a8864ddaa84032434fc2f471eb64848
cjporter25 <chris.j.porter25@gmail.com>
2024-06-05 16:41:16 -0500

    Intro

--- src/main/scala/com/rockthejvm/part1basics/Instructions.scala
+++ src/main/scala/com/rockthejvm/part1basics/Instructions.scala
@@ -7,8 +7,18 @@ object Instructions {
   //  "Imperative Programming"
   //    - task1, task2, repeat(10) {...}
   //    - We tell the computer what to do, step by step
-  //  "
+  //  "Functional Programming"
   //    - 2 + 3, I/O effects, map, flatMap, filter
+  //    - Here, expressions are evaluated
+
+  // Instructions in Scala:
+  // "Printing as an assignment to a variable"
+  //    (Even if there's nothing in main, this will
+  //    still execute because it occurs during the
+  //    assignment stage)
+  val printing = println("This is an instruction")
+  //
+
   def main(args: Array[String]): Unit = {
 
   }
