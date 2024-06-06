31e55671e9a8c3c0fe6c44c0b364a7f6c318f32e
cjporter25 <chris.j.porter25@gmail.com>
2024-06-05 19:40:48 -0500

    Intro

---
+++ src/main/scala/com/rockthejvm/part1basics/Functions.scala
@@ -0,0 +1,13 @@
+package com.rockthejvm.part1basics
+
+object Functions {
+
+
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+}
--- src/main/scala/com/rockthejvm/part1basics/Instructions.scala
+++ src/main/scala/com/rockthejvm/part1basics/Instructions.scala
@@ -16,10 +16,47 @@ object Instructions {
   //    (Even if there's nothing in main, this will
   //    still execute because it occurs during the
   //    assignment stage)
-  val printing = println("This is an instruction")
-  //
+  val printing1 = println("This is an instruction")
+  // The above and below returns a "unit". An expression that
+  //    returns a unit doesn't return a value but it does something.
+  // In other words, in Scala, an instruction is a structure (like
+  //    the thing below) that returns a unit
+  val printing2: Unit = println("This is another instruction")
+  // Units have a value which is "()". Every expression returning
+  //    unit, returns this token.
+  // If you compare two expressions that return unit, regardless of
+  //    what they're doing, it'll return true.
+  val theUnit: Unit = ()
+
+  // Instructions, in Code Block form, can also return Unit
+  val aCodeBlock = {
+    val aLocalValue = 45
+    println("Instruction 1")
+    println("Instruction 2")
+  }
+
+  // Variables in Scala. Values can't be reassigned but
+  //    variables can!
+  var aVariable = 10
+  aVariable = 34
+
+  // Variable Reassignment also return unit and can be assigned
+  //    as such.
+  // The act of reassignment won't return anything valuable but,
+  //    this assignment (because it's saved to a value), still
+  //    produces the effect of changing the memory zone.
+  val reassignment: Unit = aVariable += 1
+  val printing3: Unit = println(aVariable)
+
+
 
   def main(args: Array[String]): Unit = {
-
+    println(printing1 == theUnit)
+    // Loops in Scala
+    var theNumber = 1
+    while (theNumber <= 10) {
+      println(theNumber)
+      theNumber += 1
+    }
   }
 }
